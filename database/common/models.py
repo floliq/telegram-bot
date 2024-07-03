88524a18027359c46883624b4b64e5693cc2f30c
floliq <enboikovlad@gmail.com>
2024-07-03 14:33:06 +0300

    peewee orm created

---
+++ database/common/models.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+import peewee as pw
+
+db = pw.SqliteDatabase('database.db')
+
+
+class ModelBase(pw.Model):
+    created_at = pw.DateField(default=datetime.now())
+
+    class Meta():
+        database = db
+
+
+class History(ModelBase):
+    number = pw.TextField()
+    message = pw.TextField()
